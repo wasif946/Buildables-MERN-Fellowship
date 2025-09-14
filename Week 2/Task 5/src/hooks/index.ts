00520f48c4933deaf10e01c70878117018eeccb2
Wasif Waheed <wasifwaheed71@gmail.com>
2025-09-14 21:44:20 +0500

    Week2, Task5, Buildables MERN Fellowship

---
+++ Week 2/Task 5/src/hooks/index.ts
@@ -0,0 +1,4 @@
+// src/hooks/index.ts
+export { default as useFetch } from "./useFetch";
+export { default as useAuth } from "./useAuth";
+export { default as usePagination } from "./usePagination";

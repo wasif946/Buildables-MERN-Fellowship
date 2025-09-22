b771370ecb3e01b3c79ffedb1327c105798abfb0
Wasif Waheed <wasifwaheed71@gmail.com>
2025-09-22 22:53:26 +0500

    Week3, Task5, Buildables MERN Fellowship

---
+++ Week 3/Task 5/src/pages/UserDetail.jsx
@@ -0,0 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+
+export default function UserDetail() {
+  const user = useLoaderData();
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>Phone:</strong> {user.phone}</p>
+      <p><strong>Website:</strong> {user.website}</p>
+    </div>
+  );
+}

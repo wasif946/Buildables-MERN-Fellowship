55834a07f35bcefc509fb4b3b611254995571028
Wasif Waheed <wasifwaheed71@gmail.com>
2025-09-22 22:37:21 +0500

    Week3, Task5, Buildables MERN Fellowship

---
+++ Week 3/Task 5/src/forms/defaultValues.js
@@ -0,0 +1,10 @@
+// src/forms/defaultValues.js
+export const FORM_KEY = "myapp:multistep_form_v1";
+export const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  address: "",
+  city: "",
+  zip: "",
+};
